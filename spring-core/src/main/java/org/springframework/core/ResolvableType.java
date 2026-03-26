5708b73ea9a6dd370f79a3989b33af17773bf501
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2026-03-26 15:54:25 +0100

    Introduce ResolvableType.forParameter() factory method
    Prior to this commit, one could invoke
    ResolvableType.forMethodParameter(MethodParameter.forParameter(parameter))
    to create a ResolvableType for a Parameter; however, that's slightly
    cumbersome.
    
    To address that, this commit introduces ResolvableType.forParameter(),
    analogous to existing convenience factory methods in ResolvableType.
    
    Closes gh-36545

--- spring-core/src/main/java/org/springframework/core/ResolvableType.java
+++ spring-core/src/main/java/org/springframework/core/ResolvableType.java
@@ -22,6 +22,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
@@ -1278,6 +1279,18 @@ public class ResolvableType implements Serializable {
 		Assert.notNull(field, "Field must not be null");
 		ResolvableType owner = forType(implementationClass).as(field.getDeclaringClass());
 		return forType(null, new FieldTypeProvider(field), owner.asVariableResolver()).getNested(nestingLevel);
+	}
+
+	/**
+	 * Return a {@code ResolvableType} for the specified {@link Parameter}.
+	 * <p>This is a convenience factory method for scenarios where a {@code Parameter}
+	 * descriptor is already available.
+	 * @param parameter the source parameter
+	 * @return a {@code ResolvableType} for the specified parameter
+	 * @since 7.1
+	 */
+	public static ResolvableType forParameter(Parameter parameter) {
+		return forMethodParameter(MethodParameter.forParameter(parameter));
 	}
 
 	/**
