684b1e8a4b3df9589f3e9abb8c09a5e98a47e4b5
Juergen Hoeller <juergen.hoeller@broadcom.com>
2026-04-02 14:30:51 +0200

    Enforce strict implementation of Resource#isReadable() contract
    Closes gh-36584
    
    (cherry picked from commit e977bd7bf0efe8341a03de025ee6ac376e6a57c8)

--- spring-core/src/test/java/org/springframework/core/io/ResourceTests.java
+++ spring-core/src/test/java/org/springframework/core/io/ResourceTests.java
@@ -269,6 +269,50 @@ class ResourceTests {
 		}
 
 		@Test
+		void isReadableChecksExistsFirst() {
+			AtomicBoolean existsCalled = new AtomicBoolean();
+			AtomicBoolean canReadCalled = new AtomicBoolean();
+			File file = new File("/dev/my.txt") {
+				@Override
+				public boolean exists() {
+					existsCalled.set(true);
+					return false;
+				}
+				@Override
+				public boolean canRead() {
+					canReadCalled.set(true);
+					return false;
+				}
+			};
+			Resource resource = new FileSystemResource(file);
+			assertThat(resource.isReadable()).isFalse();
+			assertThat(existsCalled).isTrue();
+			assertThat(canReadCalled).isFalse();
+		}
+
+		@Test
+		void isReadableChecksCanReadAfterExists() {
+			AtomicBoolean existsCalled = new AtomicBoolean();
+			AtomicBoolean canReadCalled = new AtomicBoolean();
+			File file = new File("/dev/my.txt") {
+				@Override
+				public boolean exists() {
+					existsCalled.set(true);
+					return true;
+				}
+				@Override
+				public boolean canRead() {
+					canReadCalled.set(true);
+					return false;
+				}
+			};
+			Resource resource = new FileSystemResource(file);
+			assertThat(resource.isReadable()).isFalse();
+			assertThat(existsCalled).isTrue();
+			assertThat(canReadCalled).isTrue();
+		}
+
+		@Test
 		void readableChannelProvidesContent() throws Exception {
 			Resource resource = new FileSystemResource(getClass().getResource("ResourceTests.class").getFile());
 			try (ReadableByteChannel channel = resource.readableChannel()) {
